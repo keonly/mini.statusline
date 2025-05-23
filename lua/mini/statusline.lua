24389e33ab27bff76967468498b881535abd1a01
keonly <70371393+keonly@users.noreply.github.com>
2025-05-24 02:03:41 +0900

    chore(modes): change display names

--- lua/mini/statusline.lua
+++ lua/mini/statusline.lua
@@ -571,19 +571,19 @@ local CTRL_V = vim.api.nvim_replace_termcodes('<C-V>', true, true, true)
 
 -- stylua: ignore start
 H.modes = setmetatable({
-  ['n']    = { long = 'Normal',   short = 'N',   hl = 'MiniStatuslineModeNormal' },
-  ['v']    = { long = 'Visual',   short = 'V',   hl = 'MiniStatuslineModeVisual' },
-  ['V']    = { long = 'V-Line',   short = 'V-L', hl = 'MiniStatuslineModeVisual' },
-  [CTRL_V] = { long = 'V-Block',  short = 'V-B', hl = 'MiniStatuslineModeVisual' },
-  ['s']    = { long = 'Select',   short = 'S',   hl = 'MiniStatuslineModeVisual' },
-  ['S']    = { long = 'S-Line',   short = 'S-L', hl = 'MiniStatuslineModeVisual' },
-  [CTRL_S] = { long = 'S-Block',  short = 'S-B', hl = 'MiniStatuslineModeVisual' },
-  ['i']    = { long = 'Insert',   short = 'I',   hl = 'MiniStatuslineModeInsert' },
-  ['R']    = { long = 'Replace',  short = 'R',   hl = 'MiniStatuslineModeReplace' },
-  ['c']    = { long = 'Command',  short = 'C',   hl = 'MiniStatuslineModeCommand' },
-  ['r']    = { long = 'Prompt',   short = 'P',   hl = 'MiniStatuslineModeOther' },
-  ['!']    = { long = 'Shell',    short = 'Sh',  hl = 'MiniStatuslineModeOther' },
-  ['t']    = { long = 'Terminal', short = 'T',   hl = 'MiniStatuslineModeOther' },
+  ['n']    = { long = 'NORMAL',   short = 'NOR',   hl = 'MiniStatuslineModeNormal' },
+  ['v']    = { long = 'VISUAL',   short = 'VIS',   hl = 'MiniStatuslineModeVisual' },
+  ['V']    = { long = 'V-LINE',   short = 'V-L',   hl = 'MiniStatuslineModeVisual' },
+  [CTRL_V] = { long = 'V-BLOCK',  short = 'V-B',   hl = 'MiniStatuslineModeVisual' },
+  ['s']    = { long = 'SELECT',   short = 'SEL',   hl = 'MiniStatuslineModeVisual' },
+  ['S']    = { long = 'S-LINE',   short = 'S-L',   hl = 'MiniStatuslineModeVisual' },
+  [CTRL_S] = { long = 'S-BLOCK',  short = 'S-B',   hl = 'MiniStatuslineModeVisual' },
+  ['i']    = { long = 'INSERT',   short = 'INS',   hl = 'MiniStatuslineModeInsert' },
+  ['R']    = { long = 'REPLACE',  short = 'REP',   hl = 'MiniStatuslineModeReplace' },
+  ['c']    = { long = 'COMMAND',  short = 'CMD',   hl = 'MiniStatuslineModeCommand' },
+  ['r']    = { long = 'PROMPT',   short = 'PRM',   hl = 'MiniStatuslineModeOther' },
+  ['!']    = { long = 'SHELL',    short = 'SHL',   hl = 'MiniStatuslineModeOther' },
+  ['t']    = { long = 'TERMINAL', short = 'TER',   hl = 'MiniStatuslineModeOther' },
 }, {
   -- By default return 'Unknown' but this shouldn't be needed
   __index = function()
